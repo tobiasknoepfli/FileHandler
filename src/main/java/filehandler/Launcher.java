c67c8ae9355678a9ed201f5ad0bd8ff18978d2fe
Tobias Knöpfli <113889152+tobiasknoepfli@users.noreply.github.com>
2023-07-26 19:05:49 +0200

    executable Jar-File added

---
+++ src/main/java/filehandler/Launcher.java
@@ -0,0 +1,8 @@
+package filehandler;
+
+public class Launcher {
+    public static void main(String[] args) {
+        FileHandlerStartup fileHandlerStartup =new FileHandlerStartup();
+        fileHandlerStartup.main(args);
+    }
+}
